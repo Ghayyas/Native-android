5b5d9f2627aaa7d3e2ef1eb5667bc084ee84200c
Ghayyas <ghayyasmubashir@gmail.com>
2017-02-01 02:01:52 +0500

    android native

---
+++ DiceGame/app/src/main/java/com/project/dice/dicegame/MainActivity.java
@@ -0,0 +1,78 @@
+package com.project.dice.dicegame;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+    int score;
+    TextView rollResult;
+    //Button
+    Button rollButton;
+    //Random Num
+    Random rand;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+        score = 0;
+        rollResult = (TextView) findViewById(R.id.rollResult);
+        rollButton = (Button) findViewById(R.id.rollButton);
+        rand = new Random();
+        Toast.makeText(getApplicationContext(),"Wellcome to Dice",Toast.LENGTH_SHORT).show();
+    }
+
+      public void rollDice(View v){
+//          rollResult.setText("Clicked");
+          int num = rand.nextInt(6)+1;
+          String randomNum = "New Random Num Generated "+num;
+          rollResult.setText(randomNum);
+//       Toast.makeText(getApplicationContext(),randomNum,Toast.LENGTH_SHORT).show();
+      }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
