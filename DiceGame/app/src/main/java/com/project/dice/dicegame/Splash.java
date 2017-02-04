9965a66e9a549ccad816b93c052d7c41255174f1
Ghayyas <ghayyasmubashir@gmail.com>
2017-02-05 01:07:20 +0500

    full screen splash complete

--- DiceGame/app/src/main/java/com/project/dice/dicegame/Splash.java
+++ DiceGame/app/src/main/java/com/project/dice/dicegame/Splash.java
@@ -3,12 +3,21 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.app.Activity;
+import android.view.View;
 import android.widget.ImageView;
 public class Splash extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        View decorView = getWindow().getDecorView();
+        // Hide both the navigation bar and the status bar.
+        // SYSTEM_UI_FLAG_FULLSCREEN is only available on Android 4.1 and higher, but as
+        // a general rule, you should design your app to hide the status bar whenever you
+        // hide the navigation bar.
+        int uiOptions = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION;
+            decorView.setSystemUiVisibility(uiOptions);
         try {
           final Handler handler = new Handler();
             handler.postDelayed(new Runnable() {
